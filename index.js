ebcec942c7bca7bd5c0d1cb262a451f266a7dcbb
Benjamin Kinga <somethingscripted@gmail.com>
2020-07-02 16:35:43 -0500

    adding schema

--- index.js
+++ index.js
@@ -1,20 +1,64 @@
 var PrismaClient = require('@prisma/client').PrismaClient
+var GraphQLServer = require('graphql-yoga').GraphQLServer
 
 const prisma = new PrismaClient()
+const typeDefs = `
+type Query {
+    info: String!
+    getAllLists: [List!]!
+}
+
+type List {
+    createdAt: Int!
+    id: ID!
+    title: String!
+}
+`
+
+const resolvers = {
+    Query: {
+        info: () => `This is an example of a resolver`,
+        getAllLists: async (parent, args, context, info) => {
+            const data = await context.prisma.list.findMany()
+            data.map(item => {
+                delete item.createdAt // TODO: Change CreatedAt to appropriate Timestamp.
+            })
+            return data
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers,
+    context: { prisma }
+})
+
+const options = {
+    port: 8000,
+    endpoint: '/graphql',
+    subscriptions: '/subscriptons',
+    playground: '/playground'
+}
 
 async function main() {
     // const newItem = await prisma.list.update({
     //     where: {id: 1},
-    //     data: {
+    //     data: {k
     //         title: 'Okay, Boomer'
     //     }
     // })
-
-    console.log(await prisma.list.findMany())
+    server.start(options, ({ port }) =>
+        console.log(
+            `Server started, listening on localhost:${port} for incoming requests.`
+        )
+    )
 }
 
 main()
     .catch(err => {
         throw err
     })
-    .finally(async () => await prisma.disconnect())
+    .finally(async () => {
+        await prisma.disconnect()
+    })
